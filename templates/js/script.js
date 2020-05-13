c3b9ddf9d4c01e2ce9eb6b1333b4517f659b68b3
dlojea <dlojea@esei.uvigo.es>
2020-05-13 18:07:55 +0200

    Corregido error al mostrat formulario

--- templates/js/script.js
+++ templates/js/script.js
@@ -1,17 +1,17 @@
 function openPost() {
     var x = document.getElementById("postForm");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
+    if (x.style.display == "block") {
       x.style.display = "none";
+    } else {
+      x.style.display = "block";
     }
 }
 
 function openReply(a) {
     var x = document.getElementById("replyForm" + a + "");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
+    if (x.style.display == "block") {
       x.style.display = "none";
+    } else {
+      x.style.display = "block";
     }
 }
