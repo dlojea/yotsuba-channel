de7c543090e9bba32cb1458dc6c3aa9797d39bc7
dlojea <dlojea@esei.uvigo.es>
2020-05-19 23:11:38 +0200

    CAmbiada cabecera y replies

--- templates/js/script.js
+++ templates/js/script.js
@@ -6,12 +6,3 @@ function openPost() {
       x.style.display = "block";
     }
 }
-
-function openReply(a) {
-    var x = document.getElementById("replyForm" + a + "");
-    if (x.style.display == "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-}
