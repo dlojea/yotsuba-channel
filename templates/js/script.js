944b57effde38a57a2ceadc17b2e7878c96859da
dlojea <dlojea@esei.uvigo.es>
2020-05-20 01:47:16 +0200

    Ver likes

--- templates/js/script.js
+++ templates/js/script.js
@@ -6,3 +6,12 @@ function openPost() {
       x.style.display = "block";
     }
 }
+
+function openLikes(post) {
+    var x = document.getElementById("likesList" + post);
+    if (x.style.display == "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
